1026164bae62fd82737a8f8c0bd7864caf121f30
kartikprajapti123 <155372801+kartikprajapti123@users.noreply.github.com>
2024-12-05 17:23:01 +0530

    Changes

--- agency_portal/render/admin.py
+++ agency_portal/render/admin.py
@@ -160,8 +160,8 @@ class OrderAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+        # return False
 
     def get_urls(self):
         """
