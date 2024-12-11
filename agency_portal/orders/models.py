3a900d98f61c7be0ae63e6794d92ff11f4905dd1
kartikprajapti123 <155372801+kartikprajapti123@users.noreply.github.com>
2024-12-11 18:48:42 +0530

    cchnags

--- agency_portal/orders/models.py
+++ agency_portal/orders/models.py
@@ -45,6 +45,7 @@ class OrderItem(models.Model):
     deleted = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True,null=True)
     updated_at = models.DateTimeField(auto_now=True,null=True)
+    
     def __str__(self):
         return f"Item in Order #{self.order.order_number}: {self.category.name if self.category else 'No Category'}"
     
